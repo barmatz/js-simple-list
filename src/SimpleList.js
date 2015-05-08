f34137942db017facc63629fc282b1b64987a346
Or Barmatz <barmatzor@gmail.com>
2015-05-08 14:48:59 +0100

    Added comments

--- src/SimpleList.js
+++ src/SimpleList.js
@@ -5,14 +5,23 @@
   function SimpleList(container) {
     var inputField, addButton, items, counter;
 
+    /**
+     * Shorthand for creating a DOM element and appending it to the container.
+     */
     function createElement(tagName) {
       return container.appendChild(document.createElement(tagName)); 
     }
 
+    /**
+     * Refresh the counter with the current number of items.
+     */
     function refreshCounter() {
       counter.innerHTML = items.children.length + ' items in list';
     }
 
+    /**
+     * Adds an item to the DOM.
+     */
     function addItem(text) {
       var item = items.appendChild(document.createElement('li')),
           label = item.appendChild(document.createElement('span')),
@@ -30,6 +39,9 @@
       return item;
     }
 
+    /**
+     * Removes an item by index from the DOM.
+     */
     function removeItem(index) {
       var item = items.children[index];
 
@@ -38,18 +50,29 @@
       refreshCounter();
     }
 
+    /**
+     * Returns a reference to an item by index.
+     */
     function getItem(index) {
       return items.children[index];
     }
 
+    /**
+     * Add button click handler.
+     */
     function onAddButtonClick() {
       addItem(inputField.value);
       inputField.value = '';
     }
 
+    /**
+     * An item's delete button handler.
+     */
     function onItemDeleteButtonClick(event) {
       removeItem(getChildIndex(event.target.parentNode));
     }
+
+    // Check if the container argument provided to the constructor is a valid DOM element.
 
     if (!container) {
       throw new Error('Missing `container` argument');
@@ -57,6 +80,8 @@
       throw new Error('`container` is not a valid DOM element');
     }
 
+    // Create child DOM elements
+    
     inputField = createElement('input');
     inputField.placeholder = 'Add items to list';
 
@@ -69,6 +94,8 @@
     counter = createElement('div');
     refreshCounter();
 
+    // Expose DOM elements to the object scope.
+
     this.dom = {
       inputField: inputField,
       addButton: addButton,
@@ -77,6 +104,8 @@
       getItem: getItem
     };
   }
+
+  // Expose the SimpleList class to the global scope.
 
   global.SimpleList = SimpleList;
 }(window));
