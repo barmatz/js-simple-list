48004367d56b35c3839831e4f91be3b996c5f19f
Or Barmatz <barmatzor@gmail.com>
2015-05-08 08:44:45 +0100

    Added Grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        jshint: {
+            options: {
+                jshintrc: true
+            },
+            all: [
+                '**/*.{js,json}',
+                '!node_modules/**'
+            ]
+        },
+        mochaTest: {
+            test: {
+                src: 'test/**/*.js'
+            }
+        },
+        watch: {
+            options: {
+                atBegin: true
+            },
+            all: {
+                files: [
+                    '**/.jshintrc',
+                    '**/*.{js,json}',
+                    '!node_modules/**'
+                ],
+                tasks: 'default'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-mocha-test');
+
+    grunt.registerTask('default', ['lint', 'test']);
+    grunt.registerTask('lint', ['jshint']);
+    grunt.registerTask('test', ['mochaTest']);
+};
